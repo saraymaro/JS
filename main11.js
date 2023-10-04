32d7f64923154cf57f4b32c1947a583fc7c15128
Saray Magallanes <smagarodri@gmail.com>
2023-10-04 16:53:13 +0200

    exercises

---
+++ main11.js
@@ -0,0 +1,14 @@
+//A partir de la pizza sustiturs todos los elemento del array por cervezas.
+const ARRAY = ['🍔', '🌯', '🍣', '🍕', '🍜', '🍞', '🍙', '🍘', '🥑'];
+console.log(ARRAY.fill('🍺',4));
+
+const ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(ARRAY.fill(2,4));
+
+//Encontrar si existe un elemento en el array que sea una piña.
+const arr = [🍕, 🍕, 🍍, 🍕, 🍕]
+console.log(arr.some(element => element === 🍍));
+
+const arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.some(element => element === 'c'));
+
